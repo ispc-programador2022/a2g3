d1b4cd144db2f4eeb46a62999436bcbfa30477ad
lapi-rula <pirulapy@gmail.com>
2022-06-05 00:24:35 -0300

    commit

--- .github/main.py
+++ .github/main.py
@@ -1,1 +1,7 @@
-print("Prueba de add")
+print("Proyecto Integrador")
+print(".........................")
+p1=float(input("Ingrese sumando 1:"))
+p2=float(input("Ingrese sumando 2:"))
+p3=float(input("Ingrese sumando 3:"))
+total=p1+p2+p3
+print("El resultado es:",total)
