be4dcc35b099371bb43b4496cf031c3e5d43b18d
Emiliano Blanco <emilianoblanco@gmail.com>
2022-06-09 10:18:54 -0300

    Subo el ejercicio 1 terminado

---
+++ func_suma.py
@@ -0,0 +1,9 @@
+#### Ejercicio1 - Suma de dos parametros y salida por consola
+
+def suma():
+    num1 = int(input("Ingrese el primer numero: ")) 
+    num2 = int(input("Ingrese el primer numero: ")) 
+    suma = num1 + num2
+    print ("El resultado de la suma es " + str(suma))
+    
+suma()
