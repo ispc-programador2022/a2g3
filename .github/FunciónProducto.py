1c1d20142f60de652ebbce28df9265f2ec58d133
lapi-rula <pirulapy@gmail.com>
2022-06-11 16:29:51 -0300

    Final

---
+++ .github/FunciónProducto.py
@@ -0,0 +1,6 @@
+print("Función producto")
+print(".........................")
+p1=float(input("Ingrese factor 1: "))
+p2=float(input("Ingrese factor 2: "))
+total=p1*p2
+print("El resultado es:",total)
