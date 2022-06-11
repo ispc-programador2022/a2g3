12953b2fb4bd80d24cc67ef18bb57d3fa6d6c0cb
lapi-rula <pirulapy@gmail.com>
2022-06-11 16:54:23 -0300

    done

--- .github/main.py
+++
@@ -1,7 +0,0 @@
-print("Proyecto Integrador")
-print(".........................")
-p1=float(input("Ingrese sumando 1:"))
-p2=float(input("Ingrese sumando 2:"))
-p3=float(input("Ingrese sumando 3:"))
-total=p1+p2+p3
-print("El resultado es:",total)
