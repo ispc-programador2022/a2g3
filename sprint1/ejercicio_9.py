46b30a759f9de87fc17e6ba92d807a9bfeb511a6
Oscar Bocanegra <tecnicaturaispc@gmail.com>
2022-06-24 20:49:52 -0300

    volviendo hacer commit del ejercicio 9 por no guardar los cambios

--- sprint1/ejercicio_9.py
+++ sprint1/ejercicio_9.py
@@ -1,9 +1,10 @@
 #9- función p1, retorna el producto de los 2 primero 
 #más el 3er parámetros, usando las funciones anteriores.
+
 from func_producto import producto
 
 def fun_p1 (a, b, c):
-   return producto (a,b)+c
+       return producto (a,b)+c
   
 
     
